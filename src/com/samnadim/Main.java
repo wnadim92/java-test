48e575aca7533ed3105da1fe3f80f4db32af12c0
Samuel Nadim <wnadim2992@gmail.com>
2025-09-28 09:19:01 -0400

    add

--- src/com/samnadim/Main.java
+++ src/com/samnadim/Main.java
@@ -11,6 +11,7 @@ public class Main {
         System.out.println(numberOne + numberTwo);
         System.out.println(numberOne - numberTwo);
         System.out.println(numberOne * numberTwo);
+        System.out.println(numberOne * numberTwo);
         System.out.println(numberOne / numberTwo);
         System.out.println(numberOne % numberTwo);
 
