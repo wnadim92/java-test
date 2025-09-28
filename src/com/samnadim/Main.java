22c3b21d19ceb0aa85e0553edee2ab9ae36b13c8
Samuel Nadim <wnadim2992@gmail.com>
2025-09-27 21:49:57 -0400

    add

--- src/com/samnadim/Main.java
+++ src/com/samnadim/Main.java
@@ -6,23 +6,14 @@ public class Main {
 
 
     public static void main(String[] args) {
-        // primitives
-        int number1 = 20;
-        int number2 = 20;
-        int result = number1 + number2;
-        System.out.println(result);
-        double pi = 3.14;
-        boolean isCat = true;
-        char a = 'a';
-        char b = 'b';
-        char A = 'A';
-        char B = 'B';
-        byte n = 127;
-        byte o = -128;
-        short p = 32700;
-        int q = 2_000_000_000;
-        long r= 2_000_000_000;
-        double t = 3.141_592_653_414;
+        int numberOne = 10;
+        int numberTwo = 3;
+        System.out.println(numberOne + numberTwo);
+        System.out.println(numberOne - numberTwo);
+        System.out.println(numberOne * numberTwo);
+        System.out.println(numberOne / numberTwo);
+        System.out.println(numberOne % numberTwo);
+
 
     }
 
